512ae29713b5a25e2ce4d01fedd1a1ac46691728
Ezequiel Leites <leites25@gmail.com>
2019-08-10 18:06:34 +0200

    Site to React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-center position-ref full-height">
+      <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/ezmajoarg">
+        <img src={ logo } alt="Logo Ezmajo" className="logo" />
+      </a>
     </div>
   );
 }
